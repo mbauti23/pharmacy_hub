9607bea207dae0d0bf77f140f965cb3f847da022
Martin Bautista Jr <whatsnumber1@gmail.com>
2024-01-21 21:36:20 -0600

    Remove unnecessary packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nimble_code_exercise/app/pharmacy_hub_app.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   await Hive.initFlutter();
   WidgetsFlutterBinding.ensureInitialized();
-  // For Web URLs
-  setPathUrlStrategy();
   runApp(const PharmacyHubApp());
 }
